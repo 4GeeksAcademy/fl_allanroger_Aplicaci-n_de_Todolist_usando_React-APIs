52156f7b11a0b0347648e9139fe9ffb7072f832b
Allan Roger <105656076+allanrogerfl@users.noreply.github.com>
2024-01-16 20:19:16 +0000

    segundoCommit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,41 +1,42 @@
 import React, { useState, useEffect } from "react";
 
 const Home = () => {
-	const [todos, setTodos] = useState([""]);
-	const [newTodo, setNewTodo] = useState("");
+	const [todos, setTodos] = useState([]);
+	const [newTodo, setNewTodo] = useState();
 
 	const addTodo = (e) => {
 		e.preventDefault();
-		setTodos([...todos, newTodo]);
-		setNewTodo("");
+
+		let body = [...todos, {"label": newTodo, "done": false}];
+
+		fetch("https://playground.4geeks.com/apis/fake/todos/user/flallanroger", {
+			method: 'PUT',
+			body: JSON.stringify(body),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+		.then((response) => {
+			if(!response.ok) throw Error ("Error response")
+			return response.json()
+		})
+		.then((data) => {
+			console.log(data)
+			setTodos(body)
+			setNewTodo("");
+		})
+		.catch((error) => {
+			alert('Ha habido un Error! 😲')	
+			console.log(error)
+		});
+	
+	
 	};
 
 	const removeTodo = (index) => {
 		setTodos(todos.filter((_, todoIndex) => todoIndex !== index));
 	};
 
-let body = todos.concat ([{"label": inputText, "done": false}])
-
-	const updateTodos = (updatedTodos) => {
-		fetch('https://playground.4geeks.com/apis/fake/todos/user/flallanroger?Content-Type=application/json', {
-			method: 'PUT',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(updatedTodos)
-		})
-		.then((body) => {
-				e.preventDefault();
-				setTodos([...todos, newTodo]);
-				setNewTodo("");
-			};
-		})
-		.then((data) => {setTodos(data)})
-		.catch((error) => {
-			alert('Error')	
-			console.log(error)
-		});
-	}, []);
 
 	useEffect(() => {
 		fetch("https://playground.4geeks.com/apis/fake/todos/user/flallanroger", {
@@ -45,12 +46,14 @@ let body = todos.concat ([{"label": inputText, "done": false}])
 			}
 		})
 		.then((response) => {
-			if(!response.ok) throw Error ("error")
+			if(!response.ok) throw Error ("Error response")
 			return response.json()
 		})
-		.then((data) => {setTodos(data)})
+		.then((data) => {
+			setTodos(data)
+		})
 		.catch((error) => {
-			alert('Error')	
+			alert('Ha habido un Error! 😲')	
 			console.log(error)
 		});
 	}, []);
@@ -69,9 +72,9 @@ let body = todos.concat ([{"label": inputText, "done": false}])
 					/>
 						<button className="btnAdd" type="submit"></button>
 					</li>
-					{todos.map((todo, index) => (
+					{todos.map((tarea, index) => (
 						<li className="list-group-item" key={index}>
-							{todo.label} <button className="btnRemove" onClick={() => removeTodo(index)}>X</button>
+							{tarea.label} <button className="btnRemove" onClick={() => removeTodo(index)}>X</button>
 						</li>
 					))}
 					<li className="list-group-item">
@@ -85,86 +88,5 @@ let body = todos.concat ([{"label": inputText, "done": false}])
 
 export default Home;
 
-
-
-// import React, { useState, useEffect } from "react";
-
-// const Home = () => {
-// 	const [todos, setTodos] = useState([]);
-// 	const [newTodo, setNewTodo] = useState("");
-
-// 	useEffect(() => {
-// 		fetch("https://playground.4geeks.com/apis/fake/todos/user/flallanroger", {
-// 			method: 'GET',
-// 			headers: {
-// 				'Content-Type': 'application/json'
-// 			}
-// 		})
-// 		.then((response) => 
-// if(!response.ok) thow Error ("error")
-// return response.json())
-// 		.then((data) => {setTodos(data)})
-// 		.catch((error) => {
-// alert()	
-// console.log(error)});
-// 	}, []);
-
-// 	const updateTodos = (updatedTodos) => {
-// 		fetch('https://playground.4geeks.com/apis/fake/todos/user/flallanroger', {
-// 			method: 'PUT',
-// 			headers: {
-// 				'Content-Type': 'application/json',
-// 			},
-// 			body: JSON.stringify(updatedTodos)
-// 		})
-// 		.then((response) => response.json())
-// 		.then((data) => {
-// 			console.log(data);
-// 			setTodos(updatedTodos);
-// 		})
-// 		.catch((error) => console.error('Error:', error));
-// 	};
-	
-// 	const addTodo = (e) => {
-// 		e.preventDefault();
-// 		const updatedTodos = [...todos, { label: newTodo, done: false }];
-// 		updateTodos(updatedTodos);
-// 		setNewTodo("");
-// 	};
-
-// 	const removeTodo = (todoId) => {
-// 		const updatedTodos = todos.filter((todo) => todo.id !== todoId);
-// 		updateTodos(updatedTodos);
-// 	};
-
-// 	return (
-// 		<div className="container">
-// 			<h1>Todos</h1>
-// 			<form onSubmit={addTodo}>
-// 				<ul className="list-group">
-// 					<li className="list-group-item">
-// 						<input
-// 							type="text"
-// 							placeholder="What do you need to do?"
-// 							value={newTodo}
-// 							onChange={(e) => setNewTodo(e.target.value)}
-// 						/>
-// 						<button className="btnAdd" type="submit">Add</button>
-// 					</li>
-// 					{todos.map((todo) => (
-// 						<li className="list-group-item" key={todo.id}>
-// 							{todo.label} <button className="btnRemove" onClick={() => removeTodo(todo.id)}>X</button>
-// 						</li>
-// 					))}
-// 					<li className="list-group-item">
-// 						<p className="itemCounter">{todos.length} item left</p>
-// 					</li>
-// 				</ul>
-// 			</form>
-// 		</div>
-// 	);
-// };
-
-// export default Home;
 
 
